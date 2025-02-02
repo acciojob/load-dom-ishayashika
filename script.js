8adf6f35236bab325285882e2a7c1b25220b1bd3
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-02-03 03:19:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded",()=>{
 		console.log("DOM load success");
+	document.body.innerHTML="";
 });
 console.log("DOM load success");

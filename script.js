1148a5e4b3348794272c0e2efb53f2ad53ef6e2d
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-02-03 03:22:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded",()=>{
 		console.log("DOM load success");
-	document.body.innerHTML="";
+	setTimeout(() => {
+        document.body.innerHTML = ""; 
+    }, 500);
 });
 console.log("DOM load success");
